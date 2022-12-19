ce6445aeff5438f1d061463ff1aa303c01111093
FernandoRhenan <fernandorhenan9@gmail.com>
2022-12-19 19:45:01 -0300

    mensagens de tela

--- script.js
+++ script.js
@@ -8,14 +8,44 @@ function start() {
 
     enviar.addEventListener('click', enviarForm);
     cancelar.addEventListener('click', cancelarForm);
+    btnLinks()
+
 }
 
-function enviarForm(e){
+function enviarForm(e) {
     e.preventDefault()
-    console.log('ds')
+    let msg = document.getElementsByClassName('msg')[0]
+
+    let input = document.querySelectorAll('.input')
+    input[0].value = ''
+    input[1].value = ''
+    input[2].value = ''
+    console.log(input)
+   msg.style.opacity = '1'
+   setTimeout(()=>{
+    msg.style.opacity = '0'
+   }, 2000)
 }
 
-function cancelarForm(e){
+function cancelarForm(e) {
     e.preventDefault()
-    console.log('sa')
+    
+    let input = document.querySelectorAll('.input')
+    input[0].value = ''
+    input[1].value = ''
+    input[2].value = ''
+}
+
+function btnLinks(){
+
+    let notFound = document.getElementById('notFoundScreen')
+    
+    for(let i = 0; i < 5; i++){
+        document.querySelectorAll('.rodape-icons')[i].addEventListener('mouseenter', ()=>{
+            notFound.style.display = 'block'
+        })
+        document.querySelectorAll('.rodape-icons')[i].addEventListener('mouseleave', ()=>{
+            notFound.style.display = 'none'
+          })
+    }
 }
